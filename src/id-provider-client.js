4c7f7029fafcab30c689edbec1d503316b639f4a
Carlos Hernandez <carlosherandezarques@gmail.com>
2020-03-22 22:16:54 +0100

    token impl

--- src/id-provider-client.js
+++ src/id-provider-client.js
@@ -2,7 +2,13 @@ import { LitElement, html, css } from 'lit-element';
 import '@lion/button/lion-button.js';
 
 export class IdProviderClient extends LitElement {
-  
+  static get properties() {
+    return {
+      showRedirect: { type: Boolean },
+      showInfo: { type: Boolean}
+    };
+  }
+
   static get styles() {
     return css`
       .id-provider-client {
@@ -12,20 +18,56 @@ export class IdProviderClient extends LitElement {
     `;
   }
 
+  /**
+   * The connectedCallback is called when the element is inserted to the DOM.
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    
+    this._idFlowDetection();
+  }
+
   render() {
     return html`
       <div class="id-provider-client">
         <p>Id Provider Client</p>
-        <lion-button @click="${e => this.redirect(e)}">
-          Redirect to Login
-        </lion-button>
+
+        ${this.showRedirect
+          ? html`
+              <lion-button @click=${this._redirectToLogin}>
+                Redirect to Login
+              </lion-button>
+            `
+          : ""}
+        
     </div>
     `;
   }
 
-  redirect (e) {
-    console.log('redirect to id provider', e)
-    window.location = "https://id-provider.herokuapp.com/auth?client_id=idle-web-app&response_type=code&scope=openid&nonce=foobar&prompt=login"
+  _idFlowDetection() {
+    var urlParams = new URLSearchParams(window.location.search);
+    
+    if (urlParams.has('code')) {
+      this.showRedirect = false;
+      console.log("code: " +  urlParams.get('code'));
+
+      let code = urlParams.get('code')
+      fetch("https://id-provider.herokuapp.com/token?grant_type=authorization_code&code=" + code + "&redirect_uri=https://idle-web-app.herokuapp.com/cb", {
+        method: 'post'
+      })
+      .then(r => r.json())
+      .then(r => {
+        this.response = r.results;
+      });
+    }
+    else {
+      this.showRedirect = true;
+    }
+  }
+
+  _redirectToLogin () {
+    console.log('redirect to id provider');
+    window.location = "https://id-provider.herokuapp.com/auth?client_id=idle-web-app&response_type=code&scope=openid&nonce=foobar&prompt=login";
   }
 }
 
