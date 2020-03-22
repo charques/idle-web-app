f35881644ebc8bb07d1865930357d97b4da596c1
Carlos Hernandez <carlosherandezarques@gmail.com>
2020-03-22 16:51:26 +0100

    first commit - app setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# idle-web-app
+
+install dependencies:
+    $ npm install
+
+run the app:
+    $ npm run dev
+
---
+++ app.js
@@ -0,0 +1,3 @@
+import { IdleWebApp } from './src/IdleWebApp.js';
+
+customElements.define('idle-web-app', IdleWebApp);
